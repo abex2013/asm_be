3a58c703f459a156439857f00d37410c6802f905
Abebayehu Tefera <ATefera@excellerentsolutions.com>
2021-08-18 20:23:49 +0300

    Initial create Category entity and BaseEntity

---
+++ AssetManagement/Asset.Domain/Seeds/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asset.Domain.Seeds
+{
+    public abstract class BaseEntity
+    {
+        public Guid Guid { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid CreatedbyUserGuid { get; set; }
+        public Guid LastModifiedByUserGuid { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
